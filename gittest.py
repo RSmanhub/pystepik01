d66485282fa59fef7f76a6390393652392bfc3ca
RS_man <r.sagaidak@gmail.com>
2023-10-16 13:53:45 +0200

    get Class

--- gittest.py
+++ gittest.py
@@ -1,13 +1,33 @@
 """
 Файл для тренировки и учёбы
 """
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    name: str
+    age: int
+
+
+
+def get_anot(word: str, num: int) -> str:
+    """
+    Получить анотацию для слова
+    :param word: слово
+    :param num: число
+    :return: анотация
+    """
+    return f"{word} {num}"
 
 
 def main():
     print("Hello from GitHub")
     print("new commit")
 
+    person1 = Person("Peter", 20)
+    print(person1)
+
 
 if __name__ == "__main__":
     main()
-
